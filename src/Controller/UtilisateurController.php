c6f6758c2f441dfd786986f0d24a417958c8c58b
HP <chammemcinda@gmail.com>
2024-02-10 18:34:27 +0100

    second commit

---
+++ src/Controller/UtilisateurController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Utilisateur;
+use App\Form\UtilisateurType;
+use App\Repository\UtilisateurRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UtilisateurController extends AbstractController
+{
+    #[Route('/utilisateur', name: 'app_utilisateur')]
+    public function index(): Response
+    {
+        return $this->render('utilisateur/index.html.twig', [
+            'controller_name' => 'UtilisateurController',
+        ]);
+    }
+
+    #[Route('/showUtilisateur', name: 'showJoueur')]
+    public function showJoueur(UtilisateurRepository $joueurRepository): Response
+    {
+        $joueur=$joueurRepository->FindBy(array (), array ('nom' => 'ASC'));
+        $joueur=$joueurRepository->showJoueurs();
+        return $this->render('joueur/showJoueur.html.twig', [
+            'joueur' => $joueur,
+        ]);
+    }
+
+    #[Route('/addUtilisateur', name: 'addUtilisateur')]
+    public function addUtilisateur(ManagerRegistry $managerRegistry , Request $req): Response
+    {
+        $em=$managerRegistry->getManager();
+        $utilisateur=new Utilisateur;
+        $form=$this->createForm(UtilisateurType::class,$utilisateur);
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid())
+        
+        {   $em->persist($utilisateur);
+            $em->flush();
+            return $this->redirectToRoute('showUtilisateur');
+        }
+        return $this->renderForm('utilisateur/addUtilisateur.html.twig', [
+            'form' => $form ,
+        ]);
+    }
+}
