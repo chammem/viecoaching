18f2c32637e95158bd2bd2e91c9f87d004dbb001
asmahamdi600 <asmahamdi600@gmail.com>
2024-02-14 21:51:54 +0100

    Entity Seance and TypeSeance

---
+++ src/Entity/Seance.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeanceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SeanceRepository::class)]
+class Seance
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $duree = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $lien = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $mot_de_passe = null;
+
+    #[ORM\ManyToOne(inversedBy: 'seances')]
+    private ?TypeSeance $typeSeance = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDuree(): ?\DateTimeInterface
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(\DateTimeInterface $duree): static
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getLien(): ?string
+    {
+        return $this->lien;
+    }
+
+    public function setLien(string $lien): static
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->mot_de_passe;
+    }
+
+    public function setMotDePasse(string $mot_de_passe): static
+    {
+        $this->mot_de_passe = $mot_de_passe;
+
+        return $this;
+    }
+
+    public function getTypeSeance(): ?TypeSeance
+    {
+        return $this->typeSeance;
+    }
+
+    public function setTypeSeance(?TypeSeance $typeSeance): static
+    {
+        $this->typeSeance = $typeSeance;
+
+        return $this;
+    }
+}
